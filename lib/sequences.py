2a3bad4be171d91024a444af0f32ad2bb963060b
Shania Lopez <slopez@fandm.edu>
2023-08-21 05:45:38 -0400

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    if length > 0:
+        fib.append(0)
+    if length >= 2:
+        fib.append(1)
+        for i in range (2, length):
+            fib.append(fib[-1] + fib[-2])
+    print(fib)
+
+
+    
+
+# length = number of indic
